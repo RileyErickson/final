98beea7bfb0e7f59dc91c4062d71259cba6e9ab7
Riley Erickson <mommaduck36@gmail.com>
2024-11-27 17:09:00 -0500

    test commit

--- final/main.cpp
+++ final/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "stockman.h"
 int main() {
-	std::cout << "test" << std::endl;
+	std::cout << "test1" << std::endl;
 	stockman user("nitro");
 	cout << "Account Name: " << user.getAccount() << endl;
 	return 0;
