e6b78223335ecc7dc0ea950445d1b1fe02c51e62
Riley Erickson! <mommaduck36@gmail.com>
2024-11-27 22:24:42 -0500

    name test again

--- final/main.cpp
+++ final/main.cpp
@@ -5,7 +5,7 @@
 #include "viewMan.h"
 int main() {
 	//test code no real usage
-	std::cout << "test25" << std::endl;
+	std::cout << "test21" << std::endl;
 	stockman sm = stockman();
 	account user(sm);
 	bool run = true;
