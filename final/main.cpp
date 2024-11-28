6d85406b43309a96b2f9f4fc91d460ed81da47a6
Riley Erickson <mommaduck36@gmail.com>
2024-11-27 21:58:40 -0500

    fixed name from nickname to real name

--- final/main.cpp
+++ final/main.cpp
@@ -5,7 +5,7 @@
 #include "viewMan.h"
 int main() {
 	//test code no real usage
-	std::cout << "test2" << std::endl;
+	std::cout << "test3" << std::endl;
 	stockman sm = stockman();
 	account user(sm);
 	bool run = true;
