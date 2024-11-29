5a8394a8847d0d14a32db5418641867c5f04455f
Riley Erickson <mommaduck36@gmail.com>
2024-11-28 22:13:32 -0500

    cleaned up start screen/main. stopping for the day to eat ice cream

--- final/main.cpp
+++ final/main.cpp
@@ -3,13 +3,19 @@
 #include "stock.h"
 #include "account.h"
 #include "viewMan.h"
+using namespace std;
 int main() {
-	//test code no real usage
-	std::cout << "test" << std::endl;
+	string name;
 	stockman sm = stockman();
-	account user(sm, "nitro");
-	bool run = true;
+	cout << "WELCOME TO THE AMAZING STOCK SIMULATOR!!!!" << endl;
+	cout << "WHO WANTS TO BECOME RICH FROM THE STOCK MARKET!!!" << endl;
+	cout << "enter name : ";
+	cin >> name;
+	cout << endl;
+	account user(sm, name);
 	viewMan x(&user, sm);
+	//starts menu
 	x.userInterface();
 	return 0;
+	exit;
 }
